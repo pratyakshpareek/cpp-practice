98dddc8fef9fb11e1f587dbc1a4d4e733639409e
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-04-18 22:20:46 +0530

    question2

---
+++ Pracrice 1/question2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter first number ";
+    cin>>x;
+    int y;
+    cout<<"enter second number ";
+    cin>>y;
+    cout<<"the product is "<<x*y;
+}
