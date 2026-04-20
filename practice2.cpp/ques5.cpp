0b3b03576f3dd11c39137e668b3b18a3420e5cf1
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-04-20 23:51:10 +0530

    ques5

---
+++ practice2.cpp/ques5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+cout<<" enter the number ";
+cin>>x;
+int y;
+cout<<" enter the  2 number ";
+cin>>y;
+if(x>y)
+    cout<<x;
+else 
+  cout<<y;
+}
