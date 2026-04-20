35be2fe52d7b813ff9f6cf39eeb3a9cd00382854
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-04-20 23:32:50 +0530

    ques3

---
+++ practice2.cpp/ques3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int p;
+cout<<"enter the pricnciple ";
+cin>>p;
+int t;
+cout<<"enter the time  ";
+cin>>t;
+int r;
+cout<<"enter the rate of intrest" ;
+cin>>r;
+int si=(p*r*t)/100;
+cout<<"the simple intrest is "<<si;
+
+
+
+
+
+
+}
