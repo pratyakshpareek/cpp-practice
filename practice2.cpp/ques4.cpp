1a9d4a96a83df7b55b1d549f8b6eae63b55e4054
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-04-21 22:51:27 +0530

    ques6

--- practice2.cpp/ques4.cpp
+++ practice2.cpp/ques4.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
+     
     
 
+
+
+
+
+    
 }
 
    
---
+++ practice2.cpp/ques6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float  rupees;
+    cout<<"enter the amount in rupees ";
+    cin>>rupees;
+    float dollarr;
+    dollarr=rupees/80;
+    cout<<"amount in dollar is "<<dollarr;
+    return 0;
+}
