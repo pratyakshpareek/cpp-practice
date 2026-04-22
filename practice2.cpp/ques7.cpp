2e180837c87e245e158548520fae1dc642c9ae4e
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-04-22 22:17:52 +0530

    ques7

---
+++ practice2.cpp/ques7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a=0;
+    int b=1;
+    int c;
+    for(int i=1; i<=n;i++){
+ cout<<a<<"   ";
+    c=a+b;
+    a=b;
+    b=c;
+    }
+   
+    return 0;
+} 
