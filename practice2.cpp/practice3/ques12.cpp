4e3bfe12e1113b74e284b0ccc9aa4b13e0f03079
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-04-24 22:38:35 +0530

    ques12

---
+++ practice2.cpp/practice3/ques12.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int b;
+    cout<<"enter the base ";
+    cin>>b;
+    int h;
+    cout<<"enter the height ";
+    cin>>h;
+    cout<<"the area is "<<0.5*0.5*b*h;
+    return 0;
+}
