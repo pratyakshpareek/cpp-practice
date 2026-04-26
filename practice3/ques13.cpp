12aea24fbbf999e8aeef902e116ea826cb0a72f5
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-04-26 22:50:09 +0530

    ques12

---
+++ practice3/ques13.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int b;
+    cout<<" enter base ";
+    cin>>b;
+    int h;
+    cout<<" enter height  ";
+    cin>>h;
+    float area = 0.5*b*h;
+    cout<<" area of triangle is "<<area;
+
+}
