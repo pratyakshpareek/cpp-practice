f602056d403618c378b6c19a8641f22f41c509f9
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-04-28 22:51:59 +0530

    ques3

---
+++ practice3/ques3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int l;
+    cout<<"enter the lenght ";
+    cin>>l;
+    int b;
+    cout<<"enter the breadth ";
+    cin>>b;
+    cout<<"the area is "<<b*l;
+    return 0;
+}
