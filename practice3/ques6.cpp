5134708a87db5a24e2720c5c000de327ffb3c026
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-04-29 23:38:44 +0530

    ques6

---
+++ practice3/ques6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int d;
+    cout<<"enter digonal 1";
+    cin>>d;
+    int m;
+    cout<<"enter the dagonal 2";
+    cin>>m;
+    cout<<" the area of rhombus is "<<0.5*d*m;
+    return 0;
+
+}
+  
