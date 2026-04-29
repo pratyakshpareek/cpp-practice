5bf0c00d1efe262f598d54348175829124abae4d
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-04-29 23:32:55 +0530

    ques5

---
+++ practice3/ques5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int b;
+    cout<<"enter the base ";
+    cin>>b;
+    int h;
+    cout<<"enter the height ";
+    cin>>h;
+    cout<<"the area of parallelogram is "<<b*h;
+    return 0;
+}
