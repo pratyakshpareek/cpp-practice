7c1d734be128a3bf52cdb2a03e26f9be93f48fb3
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-06-07 02:35:31 +0530

    ques12

---
+++ practice3/ques12.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<" enter a";
+    cin>>a;
+    int b;
+    cout<<"enter b";
+    cin>>b;
+    cout<<a+b;
+    return a+b;;
+}
