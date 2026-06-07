6dbf746b6847e435c573f0d62fa347db5965836d
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-06-08 03:11:33 +0530

    ques1part2

---
+++ practice3/ques1part2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<" enter number jiska factorila chiaye ";
+    cin>>n;
+int f =1;
+ for(int i=1;i<=n;i++){
+    f=f*i;
+ }
+ cout<<f;
+}
