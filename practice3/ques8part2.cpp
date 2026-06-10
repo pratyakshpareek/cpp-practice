733f6961d3dddbdcd7aca02533eaac573c6a1c42
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-06-10 23:21:41 +0530

    ques8part2

---
+++ practice3/ques8part2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r;
+    cout<<" enter total runs";
+    cin>>r;
+    int o;
+    cout<<" enter total out";
+    cin>>o;
+    cout<<" the batting average is "<<r/o;
+    return 0;
+}
