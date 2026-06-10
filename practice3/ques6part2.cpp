0207a457fae2476921c8842d9eeef674e9152f34
Pratyaksh Pareek <pratyakshpareek1503@gmail.com>
2026-06-10 23:01:25 +0530

    ques6part2

---
+++ practice3/ques6part2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<" enter product price";
+    cin>>n;
+    int r;
+    cout<<" enter commisiom rate";
+    cin>>r;
+    cout<<" the commission is "<<(n*r)/100;
+    return 0;
+}
